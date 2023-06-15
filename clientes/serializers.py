3f079a1d44fd21ed9de3126b094c3b1961167345
flavioborgesnunes <flavioborgesnunes@gmail.com>
2023-06-15 14:54:11 -0300

    população de API's

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
-    
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf':'CPF Inválido'})
+        
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome':'não digite números nesse campo.'}) 
+        
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg':'O RG deve ter 9 digitos'})
+        
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':'O celeular deve seguir este modelo: 11 91234-1234'})
+        
+        return data
